0916d5dbba4ac5757cd93478a68255238eda11c7
anonymous1116 <hhyun116@gmail.com>
2025-11-03 20:37:19 -0500

    initial

--- Experiments/FN_bell.sh
+++ Experiments/FN_bell.sh
@@ -5,7 +5,7 @@
 #SBATCH --account=statdept
 #SBATCH --time=00:30:00
 #SBATCH --qos=standby
-#SBATCH --array=1               # Create a job array with indices from 1 to 10
+#SBATCH --array=1-100               # Create a job array with indices from 1 to 10
 #SBATCH --output=output_log_training/output_log_%A_%a.out
 #SBATCH --error=output_log_training/error_log_%A_%a.txt
 
@@ -29,5 +29,5 @@ seed=$((SLURM_ARRAY_TASK_ID + 1))
 #python ./Experiments/FN.py 
 #python ./benchmark/benchmark_training.py --num_training 100000 --seed 1 --task "cont_table" --N_EPOCHS 1 --layer_len 256 
 
-#python ./Experiments/FN2.py --seed $seed
-python ./Experiments/FN2.py --seed 1
+python ./Experiments/FN2.py --seed $seed
+#python ./Experiments/FN2.py --seed 
