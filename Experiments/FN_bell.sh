66438c1b61fc7287dc31dd0bf50920f7aea6b06c
anonymous1116 <hhyun116@gmail.com>
2025-12-01 22:35:34 -0500

    calibrating.py

--- Experiments/FN_bell.sh
+++ Experiments/FN_bell.sh
@@ -23,6 +23,6 @@ cd $SLURM_SUBMIT_DIR
 # Calculate seed and dim_out
 seed=$((SLURM_ARRAY_TASK_ID))
 
-python ./Experiments/FN_SA.py --seed $seed --true_sigma 2e-1
+python ./Experiments/FN_SA.py --seed $seed --true_sigma 1e-1
 
 #python ./Experiments/FN2.py --seed $seed --true_sigma 5e-1
