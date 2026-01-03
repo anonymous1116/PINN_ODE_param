792a5c7e4ca2fe5febaf84ea2f7c5ee2acabd6d3
anonymous1116 <hhyun116@gmail.com>
2026-01-03 16:23:33 -0500

    PTrans2.py

--- Experiments/FN_bell.sh
+++ Experiments/FN_bell.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=cpu
 #SBATCH --account=statdept
 #SBATCH --time=01:00:00
-#SBATCH --qos=normal
+#SBATCH --qos=standby
 #SBATCH --array=0-99               # Create a job array with indices from 1 to 10
 #SBATCH --output=output_log_training/output_log_%A_%a.out
 #SBATCH --error=output_log_training/error_log_%A_%a.txt
