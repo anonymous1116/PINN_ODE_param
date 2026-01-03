bb4b8b4938aa1a7e4703a27d0077f6e15426e87a
anonymous1116 <hhyun116@gmail.com>
2026-01-03 14:51:32 -0500

    PTrans2.py

--- Experiments/PTrans2.py
+++ Experiments/PTrans2.py
@@ -173,7 +173,7 @@ def main(args):
                             net5=FCNN(n_input_units=1, n_output_units=1, actv=nn.Tanh))
     optimizer = torch.optim.Adam(model.parameters(), lr=9e-3)  # 12e-3
     y_ind = np.arange(n)
-    train_epochs = 1000
+    train_epochs = 10000
     loss_history = []
     for epoch in range(train_epochs):
         np.random.shuffle(y_ind)
