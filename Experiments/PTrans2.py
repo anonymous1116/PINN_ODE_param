086fa92144fbcc8744c2b6b32c77b341afa4afe3
anonymous1116 <hhyun116@gmail.com>
2026-01-16 11:13:36 -0500

    PTrans2.py

--- Experiments/PTrans2.py
+++ Experiments/PTrans2.py
@@ -235,7 +235,9 @@ def main(args):
         estimate_t = torch.linspace(0., 1000., 1001)
         estimate_funcs = best_model.diff_eqs.compute_func_val(best_model.nets, [estimate_t.view(-1, 1)])
         estimate_funcs = torch.cat(estimate_funcs, dim=1)
-    trajectory_RMSE = np.sqrt(np.mean((estimate_funcs-torch.tensor(true_trajectory) )**2, axis=0))
+    print(trajectory_RMSE.size())
+    print(true_trajectory.shape)
+    trajectory_RMSE = np.sqrt(np.mean((estimate_funcs-true_trajectory )**2, axis=0))
     np.save(f"{output_dir}/results/trajectory_RMSE_withtrue_{s}.npy", trajectory_RMSE)
     
 
@@ -294,7 +296,7 @@ def main(args):
         estimate_funcs = best_model.diff_eqs.compute_func_val(best_model.nets, [estimate_t.view(-1, 1)])
         estimate_funcs = torch.cat(estimate_funcs, dim=1)
     estimate_funcs = estimate_funcs.numpy()
-    trajectory_RMSE = np.sqrt(np.mean((estimate_funcs-torch.tensor(true_trajectory ))**2, axis=0))
+    trajectory_RMSE = np.sqrt(np.mean((estimate_funcs-true_trajectory )**2, axis=0))
     #trajectory[s, :, :] = estimate_funcs
     param_results = np.array([best_model.diff_eqs.k1.data, best_model.diff_eqs.k2.data, best_model.diff_eqs.k3.data, 
                                best_model.diff_eqs.k4.data, best_model.diff_eqs.V.data, best_model.diff_eqs.Km.data])
