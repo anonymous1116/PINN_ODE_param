4d928d17bb454b4ae05eab1c828aa05501e2df6e
anonymous1116 <hhyun116@gmail.com>
2026-02-18 13:49:47 -0500

    FN_penalty.py

--- Experiments/FN_bell.sh
+++ Experiments/FN_bell.sh
@@ -24,7 +24,7 @@ cd $SLURM_SUBMIT_DIR
 seed=$((SLURM_ARRAY_TASK_ID))
 
 #python ./Experiments/FN_CV.py --seed $seed --true_sigma 2e-1
-python ./Experiments/FN_penalty.py --seed $seed --true_sigma 2e-1 --penalty 1.2e+01
+python ./Experiments/FN_CV_individual.py --seed $seed --true_sigma 2e-1 --penalty 1e+00
 #python ./Experiments/FN_CV_individual.py --seed 1 --true_sigma 2e-1 --penalty 1.0e+00
 
 #python ./Experiments/PTrans_penalty.py --seed 1 --true_sigma 1e-1 --penalty 1e+00
