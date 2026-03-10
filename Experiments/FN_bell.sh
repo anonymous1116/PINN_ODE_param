37140bbd8231eb695bb1dd5dece28ef3b8066eec
anonymous1116 <hhyun116@gmail.com>
2026-03-10 01:05:09 -0400

    NPE_run_embedding.py

--- Experiments/FN_bell.sh
+++ Experiments/FN_bell.sh
@@ -32,7 +32,7 @@ penalty=${penalty_list[$penalty_idx]}
 seed=$((SLURM_ARRAY_TASK_ID % 100))
 
 
-python ./Experiments/FN_CV_individual.py --seed $seed --true_sigma .1 --penalty $penalty
+python ./Experiments/FN_CV_individual.py --seed $seed --true_sigma 0.1 --penalty $penalty
 #python ./Experiments/FN_CV_optimal.py --seed $seed --true_sigma 0.5 
 #python ./Experiments/SIR_penalty.py --seed $seed --true_sigma 1 --penalty $penalty
 #python ./Experiments/SIR_CV_individual.py --seed $seed --true_sigma 5 --penalty $penalty
