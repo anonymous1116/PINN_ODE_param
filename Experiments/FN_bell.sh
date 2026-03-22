ed8f06db00446eecf87fd308a6ecb9bb5a315022
anonymous1116 <hhyun116@gmail.com>
2026-03-22 13:28:23 -0400

    NPE_run_embedding.py

--- Experiments/FN_bell.sh
+++ Experiments/FN_bell.sh
@@ -3,9 +3,9 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --partition=cpu
 #SBATCH --account=statdept
-#SBATCH --time=00:30:00
+#SBATCH --time=04:00:00
 #SBATCH --qos=standby
-#SBATCH --array=0-99               # Create a job array with indices from 1 to 10
+#SBATCH --array=0-599               # Create a job array with indices from 1 to 10
 #SBATCH --output=output_log_training/output_log_%A_%a.out
 #SBATCH --error=output_log_training/error_log_%A_%a.txt
 
@@ -23,7 +23,7 @@ cd $SLURM_SUBMIT_DIR
 # --- Logic for Seeds and Penalties ---
 # Define the 8 penalty values
 #penalty_list=(0.0001 0.00005)
-#penalty_list=(0.0001 0.001 0.01 0.1 1 0.0005 0.005 0.05 0.5)
+penalty_list=(0.0001 0.001 0.01 0.1 1 10)
 #penalty_list=(3 5 10 30 50 100 300 500)
 
 # Calculate which penalty to use (0 to 7)
@@ -37,10 +37,10 @@ seed=$((SLURM_ARRAY_TASK_ID % 100))
 #python ./Experiments/FN_CV_individual.py --seed $seed --true_sigma 0.05 --penalty $penalty
 #python ./Experiments/FN_CV_optimal.py --seed $seed --true_sigma 0.05 
 #python ./Experiments/FN_penalty.py --seed $seed --true_sigma 0.1 --penalty 1
-#python ./Experiments/PTrans_CV_individual.py --seed $seed --true_sigma 0.01 --penalty $penalty
+python ./Experiments/PTrans2_CV_individual.py --seed $seed --true_sigma 0.01 --penalty $penalty
 #python ./Experiments/PTrans_CV_optimal.py --seed $seed --true_sigma 0.05
 #python ./Experiments/PTrans_penalty.py --seed $seed --true_sigma 0.1 --penalty $penalty
-python ./Experiments/PTrans_penalty2.py --seed $seed --true_sigma 0.01 --penalty 0.01
+#python ./Experiments/PTrans_penalty2.py --seed $seed --true_sigma 0.01 --penalty 0.01
 
 #python ./Experiments/SIR_penalty.py --seed $seed --true_sigma 1 --penalty $penalty
 #python ./Experiments/SIR_CV_individual.py --seed $seed --true_sigma 5 --penalty $penalty
